d4a96b13cc1683db209b4b8fa1720f60b89c7d1d
Katsuragi <89945675+CHristopherAkko@users.noreply.github.com>
2021-09-25 09:57:09 -0500

    Add files via upload

---
+++ Ejercicios.py
@@ -0,0 +1,10 @@
+nombre = input ('Como te llamas?')
+print('Hola',nombre,'que tal tu curso de python')
+
+
+nombre = input ('Como te llamas?')
+print (nombre.upper())
+print (nombre.lower())
+
+a = ('Bring me the Horizon una vez dijo:"Si la gente intenta echarte abajo, eso solo significa que tu estas por encima de ellos."')
+print (a)
